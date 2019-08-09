09e4111fa0c86dc105592cef29248542b24798f6
bdomanski <domanski.brian@gmail.com>
2019-08-08 22:59:19 -0500

    test

--- components/MainPanel.js
+++ components/MainPanel.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class MainPanel extends Component {
     render() {
         return (
-            <input type="text" name="username" value="value" />
+            <label>Text</label>
         );
     }
 }

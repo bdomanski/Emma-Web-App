b4605570c0552edba002af0b7c46ee74dc197095
bdomanski <domanski.brian@gmail.com>
2019-08-08 22:31:29 -0500

    basic app

---
+++ components/MainPanel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainPanel extends Component {
+    render() {
+        return (
+            <input type="text" name="username" value="value" />
+        );
+    }
+}
+
+export default MainPanel;
